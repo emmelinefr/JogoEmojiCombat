68072662a70c9cd9464e50f11f76cae93316d92f
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2025-11-18 16:46:07 -0300

    cria main, ajusta classe luta e lutador

--- emoji-combat/src/main/java/com/backend/emojicombat/logica/Luta.java
+++ emoji-combat/src/main/java/com/backend/emojicombat/logica/Luta.java
@@ -1,5 +1,7 @@
 package com.backend.emojicombat.logica;
 
+import java.util.Random;
+
 public class Luta {
 
     private Lutador desafiante;
@@ -41,18 +43,54 @@ public class Luta {
     }
 
 
-    public void marcarLuta (Lutador desafiante, Lutador desafiado) {
-        if (desafiante.getCategoria() == desafiado.getCategoria() && desafiante != desafiado) {
+    public void marcarLuta (Lutador l1, Lutador l2) {
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2) {
             setAprovada(true);
+            desafiante = l1;
+            desafiado = l2;
 
         } else {
-            System.out.printf("Luta não pode acontecer!");
+            setAprovada(false);
+            desafiante = null;
+            desafiado = null;
         }
     }
 
     public void lutar (){
-        desafiante.apresentar();
-        desafiado.apresentar();
+        if (this.aprovada) {
+            System.out.println("\n=== DESAFIANTE ===");
+            this.desafiante.apresentar();
+            System.out.println("\n=== DESAFIADO ===");
+            this.desafiado.apresentar();
+
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); //vai gerar números: 0,1 ou 2, e guardar na variável "vencedor"
+            System.out.println("\n===== RESULTADO DA LUTA =====");
+            switch (vencedor) {
+                case 0: //empate
+                    System.out.println("Empatou!");
+                    this.desafiante.empatarLuta();
+                    this.desafiado.empatarLuta();
+                    break;
+
+                case 1: //desafiado vence
+                    System.out.println(this.desafiado.getNome() + " venceu!");
+                    this.desafiante.perderLuta();
+                    this.desafiado.ganharLuta();
+                    break;
+
+                case 2: //desafiante vence
+                    System.out.println(this.desafiante.getNome() + " venceu!");
+                    this.desafiante.ganharLuta();
+                    this.desafiado.perderLuta();
+                    break;
+            }
+            System.out.println("--------------------------------------------");
+
+
+        } else {
+            System.out.println("A luta não pode acontecer!");
+        }
 
     }
 }
